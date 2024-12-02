39ce73704997c63cfb16360cb95925e44288844e
Guido linux del <iguidoo@outlook.com>
2024-12-02 16:15:52 +0100

    routing

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/docs/:path*',
+          destination: '/docs/:path*'
+        }
+      ]
+    }
+  }
+};
 
 export default nextConfig;
