894ce8f2a53310fab72824f4b53eeaa175f5725b
Guido linux del <iguidoo@outlook.com>
2024-12-02 16:24:21 +0100

    feat(next.config): update rewrites to point to new docs deployment URL

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,14 +1,13 @@
+// apps/web/next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/docs/:path*',
-          destination: '/docs/:path*'
-        }
-      ]
-    }
+    return [
+      {
+        source: '/docs/:path*',
+        destination: `https://turborepo-docs-theta.vercel.app/:path*` // Point to docs deployment URL
+      }
+    ]
   }
 };
 
